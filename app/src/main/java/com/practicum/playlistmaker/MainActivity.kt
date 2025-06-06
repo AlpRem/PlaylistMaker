f6b55d82805c0c2718714e396d1adac7194d965f
testmm <test@me.comm>
2025-06-06 21:02:53 +0300

    add events from button in main activity

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSearch = findViewById<Button>(R.id.btn_search)
+        val btnLibrary = findViewById<Button>(R.id.btn_library)
+        val btnSetting = findViewById<Button>(R.id.btn_setting)
+
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку поиска!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnSearch.setOnClickListener(btnSearchClickListener)
+
+        btnLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку медиатеки !", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSetting.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку настроек !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
