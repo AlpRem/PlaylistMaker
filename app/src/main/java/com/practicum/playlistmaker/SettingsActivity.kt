d6be208f503db2704098e5848e2bb1b76fe754d9
testmm <test@me.comm>
2025-06-08 18:48:32 +0300

    change color in switch setting light/dark themes

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,17 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.annotation.StyleRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.practicum.playlistmaker.base.BaseActivity
+import androidx.core.content.withStyledAttributes
 
 class SettingsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +24,25 @@ class SettingsActivity : BaseActivity() {
         }
 
         arrowBackButton(R.id.arrow_back)
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_themes)
+        switchTheme.setOnCheckedChangeListener {
+                _, isChecked ->
+                    val styleRes = if (isChecked) R.style.switchStyleDark else R.style.switchStyleLight
+                    switchTheme.setStyle(styleRes)
+        }
+    }
+
+
+    @SuppressLint("ResourceType")
+    fun SwitchMaterial.setStyle(@StyleRes styleRes: Int) {
+        val attrs = intArrayOf(
+            com.google.android.material.R.attr.thumbTint,
+            com.google.android.material.R.attr.trackTint
+        )
+        context.withStyledAttributes(styleRes, attrs) {
+            thumbTintList = getColorStateList(0)
+            trackTintList = getColorStateList(1)
+
+        }
     }
 }
