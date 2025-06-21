edb1ac42ef8a87af3f672d037c80807eaa17b15f
testmm <test@me.comm>
2025-06-21 18:38:57 +0300

    search bar functionality implemented

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,7 +1,13 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -22,5 +28,42 @@ class SearchActivity : BaseActivity() {
         }
 
         arrowBackButton(R.id.arrow_back)
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearBtn = findViewById<ImageView>(R.id.clear_icon)
+        searchEditText.addTextChangedListener(implTextWatcher(clearBtn));
+        cleanText(clearBtn, searchEditText)
+
+    }
+
+    private fun cleanText(clearBtn: ImageView, editText: EditText) {
+        clearBtn.setOnClickListener{
+            editText.setText("");
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val currentView = currentFocus ?: View(this)
+            inputMethodManager?.hideSoftInputFromWindow(currentView.windowToken, 0)
+        }
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun implTextWatcher(clearBtn: ImageView): TextWatcher {
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearBtn.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        return textWatcher;
     }
 }
