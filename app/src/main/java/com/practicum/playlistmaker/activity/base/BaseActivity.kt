fb90ba949093b47bb9e63afd5be43ebb58042031
Roman <alprem2016@yandex.ru>
2025-07-17 20:09:24 +0300

    delete comment

--- app/src/main/java/com/practicum/playlistmaker/activity/base/BaseActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/activity/base/BaseActivity.kt
@@ -5,10 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity: AppCompatActivity() {
     protected fun arrowBackButton(arrowBack: Int) {
-//        findViewById<MaterialToolbar>(materialToolbar).setNavigationOnClickListener  {
-//            finish()
-//        }
-
         findViewById<ImageView>(arrowBack).setOnClickListener  {
             finish()
         }
