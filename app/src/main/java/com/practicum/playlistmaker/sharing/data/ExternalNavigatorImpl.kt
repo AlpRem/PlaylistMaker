33c84e13943709f3079d29a697e7e7bdbe77190e
Roman <alprem2016@yandex.ru>
2025-10-14 10:10:49 +0300

    added text constants

--- app/src/main/java/com/practicum/playlistmaker/sharing/data/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/sharing/data/ExternalNavigatorImpl.kt
@@ -3,32 +3,50 @@ package com.practicum.playlistmaker.sharing.data
 import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
+import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.sharing.domain.api.ExternalNavigator
 
 class ExternalNavigatorImpl(private val context: Context): ExternalNavigator {
 
-    override fun shareLink(shareAppLink: String, shareTitle: String) {
+    override fun shareLink() {
         val intent = Intent.createChooser(
             Intent(Intent.ACTION_SEND).apply {
                 type = "text/plain"
-                putExtra(Intent.EXTRA_TEXT, shareAppLink)
+                putExtra(Intent.EXTRA_TEXT, getShareAppLink())
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK},
-            shareTitle)
+            getShareTitle())
         context.startActivity(intent)
     }
 
-    override fun openLink(termsLink: String) {
-        val intent = Intent(Intent.ACTION_VIEW, termsLink.toUri())
+    override fun openLink() {
+        val intent = Intent(Intent.ACTION_VIEW, getTermLink().toUri())
         context.startActivity(intent)
     }
 
-    override fun openEmail(supportEmailData: EmailData) {
+    override fun openEmail() {
         val intent = Intent(Intent.ACTION_SENDTO).apply {
             data = "mailto:".toUri()
-            putExtra(Intent.EXTRA_EMAIL, arrayOf(supportEmailData.email))
-            putExtra(Intent.EXTRA_SUBJECT, supportEmailData.subject)
-            putExtra(Intent.EXTRA_TEXT, supportEmailData.text)
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getSupportEmailData().email))
+            putExtra(Intent.EXTRA_SUBJECT, getSupportEmailData().subject)
+            putExtra(Intent.EXTRA_TEXT, getSupportEmailData().text)
         }
         context.startActivity(intent)
+    }
+
+    private fun getShareAppLink(): String {
+        return context.getString(R.string.shared_text)
+    }
+    private fun getShareTitle(): String {
+        return context.getString(R.string.shared_title)
+    }
+
+    private fun getTermLink(): String {
+        return context.getString(R.string.agreement_url)
+    }
+
+    private fun getSupportEmailData(): EmailData {
+        return EmailData(email = context.getString(R.string.support_email),
+            subject=context.getString(R.string.support_subject),
+            text=context.getString(R.string.support_message))
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/sharing/domain/impl/SharingInteractorImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/sharing/domain/impl/SharingInteractorImpl.kt
@@ -1,38 +1,18 @@
 package com.practicum.playlistmaker.sharing.domain.impl
 
-
-import com.practicum.playlistmaker.sharing.data.EmailData
 import com.practicum.playlistmaker.sharing.domain.api.ExternalNavigator
 import com.practicum.playlistmaker.sharing.domain.api.SharingInteractor
 
 class SharingInteractorImpl(private val externalNavigator: ExternalNavigator) : SharingInteractor {
     override fun shareApp() {
-        externalNavigator.shareLink(getShareAppLink(), getShareTitle())
+        externalNavigator.shareLink()
     }
 
     override fun openTerms() {
-        externalNavigator.openLink(getTermsLink())
+        externalNavigator.openLink()
     }
 
     override fun openSupport() {
-        externalNavigator.openEmail(getSupportEmailData())
-    }
-
-    private fun getShareAppLink(): String {
-        return "https://practicum.yandex.ru/android-developer/"
-    }
-
-    private fun getShareTitle(): String {
-        return "Share APK"
-    }
-
-    private fun getSupportEmailData(): EmailData {
-        return EmailData(email = "alprem2016@yandex.ru",
-            subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker",
-            text = "Спасибо разработчикам и разработчицам за крутое приложение!")
-    }
-
-    private fun getTermsLink(): String {
-        return "https://yandex.ru/legal/practicum_offer/"
+        externalNavigator.openEmail()
     }
 }
