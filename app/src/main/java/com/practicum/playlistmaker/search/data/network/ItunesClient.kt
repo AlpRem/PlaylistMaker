9a0d3a2d64fc69263dc596b432c9eb129173bcb6
AlpRem <test@me.comm>
2025-10-26 00:32:26 +0300

    delete old code

--- app/src/main/java/com/practicum/playlistmaker/search/data/network/ItunesClient.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/data/network/ItunesClient.kt
@@ -6,15 +6,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ItunesClient(private val itunesService: ItunesService) : NetworkClient {
-//    private val BASE_URL = "https://itunes.apple.com"
-
-
-//    val retrofit: Retrofit = Retrofit.Builder()
-//        .baseUrl(BASE_URL)
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .build()
-//
-//    private val itunesService = retrofit.create(ItunesService::class.java)
 
     override fun doRequest(dto: Any): Response {
         return if (dto is TracksSearchRequest) {
