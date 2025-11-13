6adbf55d6f36712e8658ec71c8da98fade700c53
AlpRem <test@me.comm>
2025-11-13 16:49:50 +0300

    add destroy function binding

--- app/src/main/java/com/practicum/playlistmaker/library/ui/FavoritesFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/library/ui/FavoritesFragment.kt
@@ -16,16 +16,22 @@ class FavoritesFragment: Fragment() {
             return FavoritesFragment()
         }
     }
-    private lateinit var binding: FragmentFavoritesBinding
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: FavoritesViewModel by viewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
