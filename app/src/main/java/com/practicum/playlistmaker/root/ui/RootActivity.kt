b8e3b916ceaa5b1e338ee2c1e33bf12898b01886
AlpRem <test@me.comm>
2025-11-19 22:27:43 +0300

    add theme from nav_menu

--- app/src/main/java/com/practicum/playlistmaker/root/ui/RootActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/root/ui/RootActivity.kt
@@ -2,7 +2,12 @@ package com.practicum.playlistmaker.root.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.practicum.playlistmaker.databinding.ActivityRootBinding
 import com.practicum.playlistmaker.main.ui.MainFragment
 import com.practicum.playlistmaker.R
@@ -15,5 +20,18 @@ class RootActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val iVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+            binding.bottomNavigationView.visibility =
+                if (iVisible) BottomNavigationView.GONE else BottomNavigationView.VISIBLE
+            insets
+        }
     }
 }
