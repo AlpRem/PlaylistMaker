ca1bbbc8f8db995b4016e17529df3cc3860cc5b4
Roman <alprem2016@yandex.ru>
2025-11-19 15:33:36 +0300

    add navigation graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,8 @@ dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.koin.android)
     implementation(libs.androidx.fragment.ktx)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.fragment.ktx.v156)
 }
